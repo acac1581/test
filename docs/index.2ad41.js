864765e2aa3c7f3aa91430bcb500b10ebff47835
acac1581 <acac1581@gmail.com>
2023-04-14 22:06:17 +0700

    build

---
+++ docs/index.2ad41.js
@@ -0,0 +1,25 @@
+System.register(["./application.e4ade.js"], function (_export, _context) {
+  "use strict";
+
+  var Application, application;
+
+  function topLevelImport(url) {
+    return System["import"](url);
+  }
+
+  return {
+    setters: [function (_applicationE4adeJs) {
+      Application = _applicationE4adeJs.Application;
+    }],
+    execute: function () {
+      application = new Application();
+      topLevelImport('cc').then(function (engine) {
+        return application.init(engine);
+      }).then(function () {
+        return application.start();
+      })["catch"](function (err) {
+        console.error(err);
+      });
+    }
+  };
+});
