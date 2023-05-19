d6fdbe526500890f02fbd995381f73d29d0ca8c8
acac1581 <acac1581@gmail.com>
2023-05-19 15:31:12 +0700

    up 1

--- docs/index.babee.js
+++ docs/index.babee.js
@@ -1,4 +1,4 @@
-System.register(["./application.6e142.js"], function (_export, _context) {
+System.register(["./application.64094.js"], function (_export, _context) {
   "use strict";
 
   var Application, application;
@@ -8,8 +8,8 @@ System.register(["./application.6e142.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application6e142Js) {
-      Application = _application6e142Js.Application;
+    setters: [function (_application64094Js) {
+      Application = _application64094Js.Application;
     }],
     execute: function () {
       application = new Application();
