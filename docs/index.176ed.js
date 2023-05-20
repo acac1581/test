20ff3ee9ce67dab8f57b05b3d9b9e8eacc696ad0
acac1581 <acac1581@gmail.com>
2023-05-20 23:13:17 +0700

    up

---
+++ docs/index.176ed.js
@@ -0,0 +1,25 @@
+System.register(["./application.6b1e0.js"], function (_export, _context) {
+  "use strict";
+
+  var Application, application;
+
+  function topLevelImport(url) {
+    return System["import"](url);
+  }
+
+  return {
+    setters: [function (_application6b1e0Js) {
+      Application = _application6b1e0Js.Application;
+    }],
+    execute: function () {
+      application = new Application();
+      topLevelImport('cc').then(function (engine) {
+        return application.init(engine);
+      }).then(function () {
+        return application.start();
+      })["catch"](function (err) {
+        console.error(err);
+      });
+    }
+  };
+});
