e1a053062f62c51d45867ed2316f66d35e5cde9c
acac1581 <acac1581@gmail.com>
2023-05-22 03:02:54 +0700

    up

--- docs/application.87375.js
+++ docs/application.87375.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.13ed1.json';
+          this.settingsPath = 'src/settings.401dc.json';
           this.showFPS = false;
         }
 
--- docs/index.f9ce6.js
+++ docs/index.f9ce6.js
@@ -1,4 +1,4 @@
-System.register(["./application.b83bb.js"], function (_export, _context) {
+System.register(["./application.87375.js"], function (_export, _context) {
   "use strict";
 
   var Application, application;
@@ -8,8 +8,8 @@ System.register(["./application.b83bb.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationB83bbJs) {
-      Application = _applicationB83bbJs.Application;
+    setters: [function (_application87375Js) {
+      Application = _application87375Js.Application;
     }],
     execute: function () {
       application = new Application();
