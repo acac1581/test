ec592d8a59e1b0698845080a6e47bfee864ba678
acac1581 <acac1581@gmail.com>
2023-05-22 23:35:33 +0700

    up

---
+++ docs/index.e950c.js
@@ -0,0 +1,25 @@
+System.register(["./application.c0c30.js"], function (_export, _context) {
+  "use strict";
+
+  var Application, application;
+
+  function topLevelImport(url) {
+    return System["import"](url);
+  }
+
+  return {
+    setters: [function (_applicationC0c30Js) {
+      Application = _applicationC0c30Js.Application;
+    }],
+    execute: function () {
+      application = new Application();
+      topLevelImport('cc').then(function (engine) {
+        return application.init(engine);
+      }).then(function () {
+        return application.start();
+      })["catch"](function (err) {
+        console.error(err);
+      });
+    }
+  };
+});
